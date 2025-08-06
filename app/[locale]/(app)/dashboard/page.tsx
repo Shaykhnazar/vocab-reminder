9d5b572259e20417c24fbb53260bdc53df1d8585
Shaykhnazar <shayx3470941@gmail.com>
2025-08-06 10:58:24 +0500

    dashboard page now protected

--- app/[locale]/(app)/dashboard/page.tsx
+++ app/[locale]/(app)/dashboard/page.tsx
@@ -1,5 +1,8 @@
 import VocabularyDashboard from '@/components/VocabularyDashboard';
 import { getTranslations } from 'next-intl/server';
+import {getServerSession} from "next-auth/next";
+import {authOptions} from "@/lib/auth";
+import {redirect} from "next/navigation";
 
 export async function generateMetadata({ params: { locale } }: { params: { locale: string } }) {
   const t = await getTranslations({ locale, namespace: 'Dashboard.Metadata' });
@@ -10,5 +13,10 @@ export async function generateMetadata({ params: { locale } }: { params: { local
 }
 
 export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/login');
+  }
   return <VocabularyDashboard />;
 }
